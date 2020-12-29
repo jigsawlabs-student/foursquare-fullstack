9ad69855c47cbaf99f1c2c30d084f56ae01a43a7
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-12-28 20:21:19 -0500

    remove backend

--- backend/api/console.py
+++
@@ -1,5 +0,0 @@
-import squawk.src.models as models
-import squawk.src.adapters as adapters
-import squawk.src.db as db
-
-
---
+++ backend/console.py
@@ -0,0 +1,5 @@
+from api.src.models import *
+import api.src.adapters as adapters
+from api.src.db import *
+
+
